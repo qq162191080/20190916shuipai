c8b19347566ab085c27f43b61839c5d9104da835
Ray Ho <162191080@qq.com>
2019-09-16 22:54:22 +0800

    2019091616:37

--- Code_ray_201909/tk水牌20190915.py
+++ Code_ray_201909/tk水牌20190915.py
@@ -1,5 +1,35 @@
 from tkinter import *
-from PIL import *
+# 图片缩放：
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
+
+filename_A="001.png"
+filename_B="001.png"
+label_height=120
+resize_xy=(90,120)
+
+
+img = Image.open(filename_A)
+print(img.format)  # 输出图片基本信息
+print(img.mode)
+print(img.size)
+
+size_A=img.size
+resize_xy=[int((label_height*size_A[0])/size_A[1]), label_height] #保持原比例
+print(resize_xy)
+
+img_resize = img.resize(resize_xy)  # 调整尺寸
+
+print(img_resize)
+
+img_resize.save(filename_A)
+
+if img.format=="PNG":
+    filename_B=filename_A
+else:
+    img_resize.save(filename_B)
+
 
 root = Tk()
 
@@ -9,7 +39,8 @@ root = Tk()
 # print(root.winfo_screenheight())
 
 #全屏
-#root.attributes("-fullscreen", True)
+root.attributes("-fullscreen", True)
+
 
 closeApp=Canvas(root, width=10, height=10)
 closeApp.create_oval(3, 3,10,10, fill="red",width=0)
@@ -22,13 +53,12 @@ def appClose(event):
 closeApp.bind("<ButtonRelease-1>",appClose)
 
 
-photo = PhotoImage(file="001.png")
+photo = PhotoImage(file=filename_B)
 
 
+label_1=Label(root,text="Hello FishC!",height=label_height,image=photo)
 
-label_1=Label(root,text="Hello FishC!",height=200,image=photo)
 
-
-label_1.pack()
+label_1.pack(anchor=W)
 
 mainloop()
